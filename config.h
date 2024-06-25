e49c1d592498ed54ff882be98c8879685422931b
Tucker Johnson <tucker@newer.systems>
2024-06-25 00:46:28 -0400

    modules and order

--- config.h
+++ config.h
@@ -4,13 +4,13 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-memory",	10,	14},
 	{"",	"sb-cpu",		10,	18},
-	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
-	{"",	"sb-clock",	    60,	1},
-	{"",	"sb-battery",	5,	3},
+	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-internet",	5,	4},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	    60,	1},
 };
 
 // Sets delimiter between status commands.
-static char *delim = "  ";
+static char *delim = " ";
 
