5843aadca7eff336bb1500c929898cc13957c1cf
tucker johnson <tucker@newer.systems>
2024-09-21 09:12:39 -0400

    rm git

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-note-sync",	20,	12},
+/*	{"",	"sb-note-sync",	20,	12},*/
 	{"",	"sb-memory",	10,	14},
 	{"",	"sb-cpu",		10,	18},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-battery",	5,	3},
+/*	{"",	"sb-battery",	5,	3}, */
 	{"",	"sb-clock",	    60,	1},
 };
 
